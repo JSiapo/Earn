5b8706e3d8db597a7ba2a5242277f95073254466
JSiapoYawi <jsiapo@yawi.pe>
2022-07-03 13:14:10 -0500

    Add dialog

--- lib/pages/Card/AddCard/add_card_page.dart
+++ lib/pages/Card/AddCard/add_card_page.dart
@@ -1,5 +1,7 @@
+import 'package:barcode_widget/barcode_widget.dart';
 import 'package:eran_by_saving/pages/base.dart';
 import 'package:eran_by_saving/utils/responsive.dart';
+import 'package:eran_by_saving/utils/show_dialog.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
@@ -23,16 +25,8 @@ class AddCardPage extends StatelessWidget with BasePage {
                 iconSize: responsive.wp(5),
                 padding: const EdgeInsets.all(3),
                 onPressed: () async {
-                  String qr = await scan();
-                  if (qr.isUuid) {
-                    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                      content: Text(qr),
-                    ));
-                  } else {
-                    ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
-                      content: Text("No se recibio codigo"),
-                    ));
-                  }
+                  //await scanQr(context);
+                  showCustomDialog(context, dialogChild());
                 },
                 icon: const FaIcon(FontAwesomeIcons.qrcode),
               ),
@@ -42,7 +36,34 @@ class AddCardPage extends StatelessWidget with BasePage {
   }
 }
 
-Future<String> scan() async {
+Widget dialogChild() {
+  return BarcodeWidget(
+    decoration: BoxDecoration(
+      borderRadius: BorderRadius.circular(20),
+    ),
+    // color: const Color.fromARGB(255, 255, 255, 255),
+    barcode: Barcode.qrCode(),
+    data: "",
+    width: 300,
+    height: 300,
+    padding: const EdgeInsets.all(15.0),
+  );
+}
+
+Future<void> scanQr(BuildContext context) async {
+  String qr = await scanResult();
+  if (qr.isUuid) {
+    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+      content: Text(qr),
+    ));
+  } else {
+    ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+      content: Text("No se recibio codigo"),
+    ));
+  }
+}
+
+Future<String> scanResult() async {
   try {
     String scanResult = await FlutterBarcodeScanner.scanBarcode(
         "#f6d15f", "Cancelar", false, ScanMode.QR);
---
+++ lib/utils/show_dialog.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+void showCustomDialog(BuildContext context, Widget childWidget) {
+  showGeneralDialog(
+    context: context,
+    barrierLabel: "Barrier",
+    barrierDismissible: true,
+    barrierColor: Colors.black.withOpacity(0.5),
+    transitionDuration: const Duration(milliseconds: 700),
+    pageBuilder: (_, __, ___) {
+      return Center(
+        child: Container(
+          height: 240,
+          child: const SizedBox.expand(child: FlutterLogo()),
+          margin: const EdgeInsets.symmetric(horizontal: 20),
+          decoration: BoxDecoration(
+              color: Colors.white, borderRadius: BorderRadius.circular(40)),
+        ),
+      );
+    },
+    transitionBuilder: (_, anim, __, child) {
+      Tween<Offset> tween;
+      if (anim.status == AnimationStatus.reverse) {
+        tween = Tween(begin: const Offset(-1, 0), end: Offset.zero);
+      } else {
+        tween = Tween(begin: const Offset(1, 0), end: Offset.zero);
+      }
+
+      return SlideTransition(
+        position: tween.animate(anim),
+        child: FadeTransition(
+          opacity: anim,
+          child: child,
+        ),
+      );
+    },
+  );
+}
